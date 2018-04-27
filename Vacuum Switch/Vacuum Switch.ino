e65214170fdbc1908e82b05d3a1e60b851bf25c1
Terence Ang <terenceang@mac.com>
2018-04-27 12:12:40 +0800

    Commit

--- Vacuum Switch/Vacuum Switch.ino
+++ Vacuum Switch/Vacuum Switch.ino
@@ -1,15 +1,46 @@
-/*
- Name:		Vacuum_Switch.ino
- Created:	4/24/2018 11:44:10 PM
- Author:	Terence
-*/
 
-// the setup function runs once when you press reset or power the board
+#define THR 10 // sensor Threshold
+#define onDLY 300 // Turn On delay = 1sec
+#define offDLY 6000 // Turn Off delay = 5sec
+#define HoldValue 700 // how long before switch off delay is activated.
+#define SSR D1
+
+int currentValue = 0;
+
+// the setup routine runs once when you press reset:
 void setup() {
-
+	// initialize serial communication at 9600 bits per second:
+	Serial.begin(115200);
+	pinMode(SSR, OUTPUT);
 }
 
-// the loop function runs over and over again until power down or reset
+uint16_t sensorValue = 0; 
+// the loop routine runs over and over again forever:
 void loop() {
-  
+
+	sensorValue = analogRead(A0);
+
+	if (sensorValue > THR) {
+		currentValue += 5;
+		if (currentValue >= 700) currentValue = 6000;
+	}
+	else {
+		currentValue--;
+		if (currentValue <= 0) currentValue = 0;
+	}
+
+	Serial.print(sensorValue);
+	Serial.print(" : ");
+	Serial.println(currentValue);
+
+	if (currentValue > onDLY) {
+		digitalWrite(SSR, HIGH);
+	}
+	else
+	{
+		digitalWrite(SSR, LOW);
+	}
+
 }
+
+
